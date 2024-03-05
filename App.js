649c7cb90b0ea50981343f22cb75452e9fa3e702
Pragya Sharma <pragya@joveo.com>
2024-03-05 17:28:44 +0530

    init

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id:"heading", className:'pragya'}, "Hello from Pragya");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
